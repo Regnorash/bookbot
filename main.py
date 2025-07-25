373c943d2613048a7dce923eff7efe716c0c0e1a
Regnorash <bram_vd_horst@hotmail.com>
2025-07-25 16:15:16 +0200

    added function that counts the amount of times each character appears in the book

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from stats import count_words
+from stats import count_characters
 
-#open and read the file in the provided path
+#open and read the provided file
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
 def main():
-    #count and print the amount of words in Frankenstein
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    book_path = "books/frankenstein.txt"
+
+    #get the full text of the book
+    book_text = get_book_text(book_path)
+
+    #count the amount of words in the book
+    num_words = count_words(book_text)
+
+    #count the amount of times each character appears in the book
+    characters_count = count_characters(book_text)
+
+    #print the results
     print(f"{num_words} words found in the document")
+    print(characters_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
-#count the amount of words in the provided text
+#count the amount of words in the input text
 def count_words(text):
     return len(text.split())
+
+#count the amount of times each character appears in the input text
+def count_characters(text):
+    characters_count = {}
+    lowercase = text.lower()
+
+    for c in lowercase:
+        if not c in characters_count:
+            characters_count[c] = 0
+        characters_count[c] += 1
+    
+    return characters_count
